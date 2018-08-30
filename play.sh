7d7dbff75aabc3c556908dca1dd1538f65ec043c
Juan Andres <juanandres.s@tcs.com>
2018-08-30 10:14:01 -0300

    fast image

--- play.sh
+++ play.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+
+
 gst-launch-1.0 udpsrc port=5000 ! 'application/x-rtp,encoding-name=JPEG,payload=26,framerate=(fraction)1/1' ! rtpjpegdepay ! jpegdec ! autovideosink sync=false low-latency=1 -v
+#gst-launch-1.0 udpsrc port=5000 caps="image/jpeg, width=(int)720, height=(int)480, framerate=(fraction)30/1"  ! rtpjpegdepay ! jpegdec ! autovideosink sync=false low-latency=1 -v
